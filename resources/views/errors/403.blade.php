0f78ccf0995c8e5886bc0464b733a8b642576ff1
Hirohisa Kawase <hirokws@gmail.com>
2015-12-12 19:09:45 +0900

    追加の403、500エラーページにパンくずリスト追加、html調整

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -7,17 +7,19 @@
 アクセスが拒否されました
 @endsection
 
+@section('breadcrumb')
+<li class="active">403</li>
+@endsection
+
 @section('content')
-<div class="pure-u-1">
-  <h1>アクセスが拒否されました。(403)</h1>
-  {{--
-    abortヘルパーでコードと共にメッセージが渡された場合は、それを優先し表示する。
-    メッセージが指定されていない場合はデフォルトのメッセージを表示する。
-  --}}
-  <p>{{ $exception->getMessage() ? : 'アクセスできません。' }}</p>
-</div>
-<div class="pure-u-1">
-  {{-- トップページへ戻る --}}
-  <a class="pure-button" href="{!! url('/') !!}">トップページへ戻る</a>
+<div class="row">
+  <div class="col-md-12">
+    <h3>アクセスが拒否されました。(403)</h3>
+    {{--
+      abortヘルパーでコードと共にメッセージが渡された場合は、それを優先し表示する。
+      メッセージが指定されていない場合はデフォルトのメッセージを表示する。
+    --}}
+    <p>{{ $exception->getMessage() ? : 'アクセスできません。' }}</p>
+  </div>
 </div>
 @endsection
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -7,22 +7,24 @@
 内部エラーが発生しました
 @endsection
 
+@section('breadcrumb')
+<li class="active">500</li>
+@endsection
+
 @section('content')
-<div class="pure-u-1">
-  <h1>内部エラーが発生しました。(500)</h1>
-  {{--
-    abortヘルパーでコードと共にメッセージが渡された場合は、それを優先し表示する。
-    メッセージが指定されていない場合はデフォルトのメッセージを表示する。
-  --}}
-  @if ($exception->getMessage())
-  <p>{{ $exception->getMessage() }}</p>
-  @else
-  <p>内部的な障害のため、ただ今アクセスできません。</p>
-  <p>対策が完了するまで、しばらくお待ちください。</p>
-  @endif
-</div>
-<div class="pure-u-1">
-  {{-- トップページへ戻る --}}
-  <a class="pure-button" href="{!! url('/') !!}">トップページへ戻る</a>
+<div class="row">
+  <div class="col-md-12">
+    <h3>内部エラーが発生しました。(500)</h3>
+    {{--
+      abortヘルパーでコードと共にメッセージが渡された場合は、それを優先し表示する。
+      メッセージが指定されていない場合はデフォルトのメッセージを表示する。
+    --}}
+    @if ($exception->getMessage())
+    <p>{{ $exception->getMessage() }}</p>
+    @else
+    <p>内部的な障害のため、ただ今アクセスできません。</p>
+    <p>対策が完了するまで、しばらくお待ちください。</p>
+    @endif
+  </div>
 </div>
 @endsection
